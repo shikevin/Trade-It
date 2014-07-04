b6bd9388be6c18f5228e01b02d145daacb11a681
shikevin <im@shikev.in>
2014-07-03 23:37:25 -0400

    Refactored tests, patched lins that failed tests after ch. 5 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,10 @@
-def full_title(page_title)
-  base_title = "First Rails App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+#
+# def full_title(page_title)
+#   base_title = "First Rails App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
