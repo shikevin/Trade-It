130fe0a48a6f132164c3866e062c014876ff0a23
shikevin <im@shikev.in>
2014-07-04 01:13:03 -0400

    added uniqueness check on email attribute for database and performed migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase } #callback to force downcase email before saving
+
   validates :name, presence: true, length: { maximum: 50 } #same as validates(:name, presence : true)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
