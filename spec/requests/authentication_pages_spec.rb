07417318cad25f059d155f37f650eed2f3c93be9
shikevin <im@shikev.in>
2014-07-20 14:11:34 -0400

    Added user list and access restrictions for signed in users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,6 +29,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
@@ -63,9 +64,13 @@ describe "Authentication" do
       end
 
       describe "in the Users controller" do
-
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
           it { should have_title('Sign in') }
         end
 
