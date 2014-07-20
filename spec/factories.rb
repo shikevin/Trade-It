1eb1972b8e569ca0b5a765ce9c3c541027a90a91
shikevin <im@shikev.in>
2014-07-20 14:52:41 -0400

    added pagination for the project and for users using will_paginate gem

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name    "Kevin Shi"
-    email   "kevinshi27@hotmail.com"
-    password  "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
     password_confirmation "foobar"
   end
 end
