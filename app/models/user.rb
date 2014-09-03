672ae54bef4dc76946d55dc6cc1cf23cbb46678b
shikevin <im@shikev.in>
2014-09-02 21:27:00 -0400

    changed some content to reflect trade it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase } #callback to force downcase email before saving
   before_create :create_remember_token
 
+  has_many :microposts, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 } #same as validates(:name, presence : true)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
