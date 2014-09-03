4f85103a0c3ae9cb8d190cba11cbeeb6e2791a74
shikevin <im@shikev.in>
2014-09-02 21:45:02 -0400

    changed title, removed image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns full title on per-page basis
   def full_title(page_title)
-    base_title = "First Rails App"
+    base_title = "Trade It!"
     if page_title.empty?
       base_title
     else
