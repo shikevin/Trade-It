f0ec75638ee67947e9c9d0e24ced7ab892017398
shikevin <im@shikev.in>
2014-09-16 19:28:08 -0400

    adding facebook signin with callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,21 +15,21 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:facebook]
   # attr_accessible :password, :password_confirmation
 
-  # def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  #       user.email = data["email"] if user.email.blank?
-  #     end
-  #   end
-  # end
-  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = ActiveSupport::JSON.decode(access_token.get('https://graph.facebook.com/me?'))
-    if user = User.find_by_email(data["email"])
-      user
-    else # Create an user with a stub password.
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
+  # def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+  #   data = ActiveSupport::JSON.decode(access_token.get('https://graph.facebook.com/me?'))
+  #   if user = User.find_by_email(data["email"])
+  #     user
+  #   else # Create an user with a stub password.
+  #     User.create!(:email => data["email"], :password => Devise.friendly_token[0,20])
+  #   end
+  # end
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
