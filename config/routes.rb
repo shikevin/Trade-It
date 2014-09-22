09c69e60a714fd13bd2d61566a40c6124e430521
shikevin <im@shikev.in>
2014-09-21 20:49:26 -0400

    heroku should be working now

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :sellingbooks, only: [:create, :destroy]
   resources :wantedbooks, only: [:create, :destroy]
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users
+  # devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
